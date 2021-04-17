d2a83d40100b538d3dbf07d6faf1a58a97938923
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-04-17 13:01:49 +0600

    add service api done with monogodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
+const fs = require('fs-extra');
 const fileUpload = require('express-fileupload');
 const port = 5055;
 require('dotenv').config()
@@ -19,8 +20,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const serviceCollection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_COLLECTION}`);
+  const serviceCollection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_SERVICES}`);
   // perform actions on the collection object
+  // add service items
+  app.post('/addService', (req, res) => {
+      const file = req.files.file;
+      const title = req.body.title;
+      const price = req.body.price;
+      const description = req.body.description;
+      const filePath = `${__dirname}/services/${file.name}`;
+      file.mv(filePath, err => {
+          if(err){
+              console.log(err);
+              res.status(500).send({msg: 'Failed to upload'});
+          }
+          const newImg = fs.readFileSync(filePath);
+          const encImg = newImg.toString('base64');
+
+          var image = {
+              contentType: req.files.file.mimetype,
+              size: req.files.file.size,
+              img: Buffer(encImg, 'base64')
+          }
+
+          serviceCollection.insertOne({title, price, description, image})
+          .then(result => {
+              fs.remove(filePath, error => {
+                  if(error){
+                    console.log(err);
+                    res.status(500).send({msg: 'Failed to upload'});
+                  }
+                  res.send(result.insertedCount > 0);
+              })
+          })
+      })
+  })
  //  client.close();
 });
 
